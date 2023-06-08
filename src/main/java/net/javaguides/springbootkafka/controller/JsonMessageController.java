e9035500999e3b8d0e8838b1512f6a9ee11e0f58
JorgeFerreira <ferlzjordan@hotmail.com>
2023-06-08 16:34:27 -0400

    consumer & producer implemented

--- src/main/java/net/javaguides/springbootkafka/controller/JsonMessageController.java
+++ src/main/java/net/javaguides/springbootkafka/controller/JsonMessageController.java
@@ -1,4 +1,14 @@
 package net.javaguides.springbootkafka.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/kafka")
 public class JsonMessageController {
+
+    private JsonKafkaProducer kafkaProducer;
+
+    pu
+
 }
--- src/main/java/net/javaguides/springbootkafka/kafka/KafkaProducer.java
+++ src/main/java/net/javaguides/springbootkafka/kafka/KafkaProducer.java
@@ -3,6 +3,7 @@ package net.javaguides.springbootkafka.kafka;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -11,16 +12,19 @@ import java.util.List;
 @Service
 public class KafkaProducer {
 
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
 
     private KafkaTemplate<String, String> kafkaTemplate;
-    @Autowired
-    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate){
-        this.kafkaTemplate=kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     public void sendMessage(String message){
         LOGGER.info(String.format("Message sent %s", message));
-        kafkaTemplate.send("javaguides", message);
+        kafkaTemplate.send(topicName, message);
     }
 }
